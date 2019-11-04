b03d3fafad7fd04b758a9fdee830c31d6dfb1dbe
Marcus Björkman <marcus.bjorkman@hotmail.com>
2019-11-04 19:23:15 +0100

    initial WITH .gitignore ...

---
+++ Laboration1/Uppgift1/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Uppgift1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMessage_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Klickad :)");
+        }
+    }
+}
